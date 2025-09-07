1999c50f7ac8f5e234e15b49379fa05457a1bfa5
AlphaKore-create <alpha.kore25@gmail.com>
2025-09-07 22:17:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import {
 import alphakoreImage from "./assets/images/alphakore.png";
 import hitarthImage from "./assets/images/hitarth.jpg";
 import parthImage from "./assets/images/parth.jpg";
-import petAiImage from "./assets/images/PET.ai.png";
+import petAiImage from "./assets/images/PetAi.png";
 import synthAiImage from "./assets/images/Synth.png";
 import rajImage from "./assets/images/raj.jpg";
 import shlokImage from "./assets/images/shlok.jpg";
